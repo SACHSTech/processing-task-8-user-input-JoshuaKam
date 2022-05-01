02dc5f9bc3d534758c05b8a887e730a1a2d58994
JoshuaKam <joshua.kam24@ycdsbk12.ca>
2022-05-01 18:47:22 -0400

    Finished Task 8

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,112 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  int x;
+  int y;
+  float circleX = 200;
+  float circleY = 200;
+	String message = "";
+ 
+
   public void settings() {
 	// put your size call here
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+ 
   public void setup() {
-    background(210, 255, 173);
+    background(173,216,230);
+    textSize(20);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
+   //grass
+  public void mouseDragged(){
+      stroke(0,255,0);
+    float rand = random(height/30*15,height/30*19);
+    line(mouseX,375, mouseX, rand);
+    
+  }
+
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+fill(0,255,0);
+    rect(0,375,width,height);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    
+    //moving circle
+  if (keyPressed) {
+    if (keyCode == UP) {
+      circleY--;
+    } 
+    else if (keyCode == DOWN) {
+      circleY++;
+    } 
+    else if(keyCode == LEFT){
+      circleX--;
+    }
+    else if(keyCode == RIGHT){
+     circleX++; 
+
+     ellipse(circleX, circleY, 50, 50);
+    }
+  } 
+
+    //background colour
+    if(keyPressed){
+
+      if(key == '1' ){
+        background(230,230,250);
+      }
+    else if(key == '2'){
+     background(255,0,0);
+    }
+     else if(key == '3'){
+       background(255,20,147);
+     }
+     }
+
+
+
+
+//flower
+if (keyPressed){
+translate(width/4*3, height/4*3);
+   for(int c = 0; c <= 3; c++){
+     stroke(255,140,0);
+   fill(255,140,0);
+  ellipse(0,0,30,110);
+   fill(0,100,0);
+  ellipse(0, 0, width/40*5, height/40*5);
+  rotate(PI/4);
+   }
 }
+}
+  
+  //sun
+public void mouseWheel(){
+fill(255,255,0);
+ellipse(400,0,100,100);
+  }
+
+ 
+
+  //clouds
+  public void mouseClicked(){
+    fill(220,220,220);
+    ellipse(mouseX,mouseY,50,50);
+  }
+
+  public void keyReleased(){
+    translate(width/4*3, height/4*3);
+   for(int c = 0; c <= 3; c++){
+   fill(255,140,0);
+  ellipse(0,0,30,110);
+   fill(0,100,0);
+  ellipse(0, 0, width/40*5, height/40*5);
+  rotate(PI/4);
+   }
+}
+  }
+
+
+
+  // define other methods down here.
